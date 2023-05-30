039eb86331994a0c1bcecfb7fe295e1377f3932d
hhjj55013 <hhjj55013@gmail.com>
2023-05-30 11:49:27 -0700

    update for assignment4-2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ./conf/assignment.txt`
+assignment='assignment4'
 
 if [ $assignment != 'assignment1' ]
 then
